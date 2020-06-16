bace2224701099e73037be0720f880e78383a1ee
Yann Sommer <ysommer@gmail.com>
2020-06-16 10:19:23 -0500

    Prototype

---
+++ index.js
@@ -0,0 +1,73 @@
+const Service, Characteristic;
+
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("switch-plugin", "HoseValve", myValve);
+};
+
+myValve.prototype = {
+  getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Melnor")
+      .setCharacteristic(Characteristic.Model, "RainCloud")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+    let switchService = new Service.Switch("Rain Cloud");
+    switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getSwitchOnCharacteristic.bind(this))
+      .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
+};
+
+const request = require('request');
+const url = require('url');
+
+function myValve(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+
+myValve.prototype = {
+
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+      url: me.getUrl,
+      method: 'GET',
+    },
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
